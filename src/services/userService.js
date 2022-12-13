fba8ae96e8f88bf17b02a3fe5a009725ecceaeef
HoLySoi <45162745+HoLySoi@users.noreply.github.com>
2022-12-13 21:49:03 +0700

    #38

--- src/services/userService.js
+++ src/services/userService.js
@@ -7,4 +7,8 @@ const handleLoginApi = (userEmail, userPassword) => {
   });
 };
 
-export { handleLoginApi };
+const getAllUsers = (inputId) => {
+  return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+
+export { handleLoginApi, getAllUsers };
