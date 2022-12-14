bf9af7671cb094247fa1c9450606d57f8ccc5188
HoLySoi <45162745+HoLySoi@users.noreply.github.com>
2022-12-14 23:40:24 +0700

    #42

--- src/services/userService.js
+++ src/services/userService.js
@@ -15,5 +15,12 @@ const createNewUserService = (data) => {
   console.log("check data from service:", data);
   return axios.post("/api/create-new-user", data);
 };
+const deleteUserService = (userId) => {
+  return axios.delete("/api/delete-user", {
+    data: {
+      id: userId,
+    },
+  });
+};
 
-export { handleLoginApi, getAllUsers, createNewUserService };
+export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService };
