ef4a15d7e814d4ad58e54d9088a7e8adc08eb258
HoLySoi <45162745+HoLySoi@users.noreply.github.com>
2022-12-16 01:04:22 +0700

    #48

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import HomeHeader from "./HomeHeader";
+import Specialty from "./Section/Specialty";
 
 class HomePage extends Component {
   render() {
     return (
       <div className="text-center">
         <HomeHeader />
+        <Specialty />
+        <div style={{ height: "600px" }}></div>
       </div>
     );
   }
