8eb41040efd05c5eb3119a63a46621f571aa3b86
HoLySoi <45162745+HoLySoi@users.noreply.github.com>
2022-12-15 12:49:13 +0700

    #45

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -5,40 +5,92 @@ import "./HomeHeader.scss";
 class HomeHeader extends Component {
   render() {
     return (
-      <div className="home-header-container">
+      <React.Fragment>
         <div className="home-header-container">
-          <div className="left-content">
-            <i className="fas fa-bars"></i>
-            <div className="header-logo"></div>
-          </div>
-          <div className="center-content">
-            <div className="child-content">
-              <div>
-                <b>Chuyeen khoa</b>
+          <div className="home-header-container">
+            <div className="left-content">
+              <i className="fas fa-bars"></i>
+              <div className="header-logo"></div>
+            </div>
+            <div className="center-content">
+              <div className="child-content">
+                <div>
+                  <b>Chuyeen khoa</b>
+                </div>
+                <div className="subs-title">Tim bac si theo chuyeen khoa</div>
               </div>
-              <div className="subs-title">Tim bac si theo chuyeen khoa</div>
-            </div>
-            <div className="child-content">
-              <div>
-                <b>Chuyeen khoa</b>
+              <div className="child-content">
+                <div>
+                  <b>Chuyeen khoa</b>
+                </div>
+                <div className="subs-title">Tim bac si theo chuyeen khoa</div>
               </div>
-              <div className="subs-title">Tim bac si theo chuyeen khoa</div>
-            </div>
-            <div className="child-content">
-              <div>
-                <b>Goi kham</b>
+              <div className="child-content">
+                <div>
+                  <b>Goi kham</b>
+                </div>
+                <div className="subs-title">Kham suc khoe tong quat</div>
               </div>
-              <div className="subs-title">Kham suc khoe tong quat</div>
             </div>
-          </div>
-          <div className="right-content">
-            <div className="support">
-              <i className="fas fa-question-circle"></i>Hoox tro
+            <div className="right-content">
+              <div className="support">
+                <i className="fas fa-question-circle"></i>Hoox tro
+              </div>
+              <div className="flag">Vn</div>
             </div>
-            <div className="flag">Vn</div>
           </div>
         </div>
-      </div>
+        <div className="home-header-banner">
+          <div className="content-up">
+            <div className="title1">NỀN TẢNG Y TẾ</div>
+            <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+            <div className="search">
+              <i className="fas fa-search"></i>
+              <input type="text" placeholder="Tìm chuyên khoa khám bệnh" />
+            </div>
+          </div>
+          <div className="content-down">
+            <div className="options">
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="far fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám chuyên khoa</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="far fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám chuyên khoa</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="far fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám chuyên khoa</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="far fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám chuyên khoa</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="far fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám chuyên khoa</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="far fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám chuyên khoa</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
