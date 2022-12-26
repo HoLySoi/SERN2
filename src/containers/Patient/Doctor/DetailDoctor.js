f1b1e59e4804a3d7fa43958cdd7645142eca793d
HoLySoi <45162745+HoLySoi@users.noreply.github.com>
2022-12-26 12:32:59 +0700

    #75

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -4,6 +4,7 @@ import HomeHeader from "../../HomePage/HomeHeader";
 import "./DetailDoctor.scss";
 import { getDetailInforDoctor } from "../../../services/userService";
 import { LANGUAGES } from "../../../utils";
+import DoctorSchedule from "./DoctorSchedule";
 
 class DetailDoctor extends Component {
   constructor(props) {
@@ -78,7 +79,17 @@ class DetailDoctor extends Component {
               </div>
             </div>
           </div>
-          <div className="schedule-doctor"></div>
+          <div className="schedule-doctor">
+            <div className="content-left">
+              <DoctorSchedule
+                doctorIdFromParent={
+                  detailDoctor && detailDoctor.id ? detailDoctor.id : -1
+                }
+              />
+            </div>
+
+            <div className="content-right"></div>
+          </div>
           <div className="detail-infor-doctor my-3">
             {detailDoctor &&
               detailDoctor.Markdown &&
--- src/services/userService.js
+++ src/services/userService.js
@@ -52,6 +52,13 @@ const getDetailInforDoctor = (inputId) => {
 const saveBulkScheduleDoctor = (data) => {
   return axios.post(`/api/bulk-create-schedule`, data);
 };
+
+const getScheduleDoctorByDate = (doctorId, date) => {
+  return axios.get(
+    `/api/get-schedule-doctor-by-date?doctorId=${doctorId}&date=${date}`
+  );
+};
+
 export {
   handleLoginApi,
   getAllUsers,
@@ -64,4 +71,5 @@ export {
   saveDetailDoctorService,
   getDetailInforDoctor,
   saveBulkScheduleDoctor,
+  getScheduleDoctorByDate,
 };
