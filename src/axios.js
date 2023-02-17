f9107bf58720c8e23b58895ed83773bc4b30465e
minhvh <minhvh@sapo.vn>
2023-02-17 16:09:46 +0700

    sua axios cors

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  // withCredentials: true
+  withCredentials: false,
 });
 
 instance.interceptors.response.use((response) => {
